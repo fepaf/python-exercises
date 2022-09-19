d0599e827b180285401e8e177fec019f490d802d
Francisco Elio Parente Arcos Filho <francisco.fepaf@gmail.com>
2022-09-19 14:42:26 -0400

    fix: Fix fucntion name

--- frequency-sort.py
+++ frequency-sort.py
@@ -1,6 +1,6 @@
-def f(a):
+def frequency_sort(items):
     d = {}
-    for ai in a:
+    for ai in items:
         d[ai] = d.get(ai, 0)+1
     ordered_pairs = sorted(d.items(), key=lambda x : (-x[1],x[0]))
     ans = []
@@ -8,7 +8,7 @@ def f(a):
         ans.extend(v*[k])
     return ans
 
-print(f([4, 6, 2, 2, 6, 4, 4, 4]))
-print(f([4, 6, 1, 2, 2, 1, 1, 6, 1, 1, 6, 4, 4, 1]))
-print(f([17, 99, 42]))
-print(f(['bob','bob','carl','alex','bob']))
+print(frequency_sort([4, 6, 2, 2, 6, 4, 4, 4]))
+print(frequency_sort([4, 6, 1, 2, 2, 1, 1, 6, 1, 1, 6, 4, 4, 1]))
+print(frequency_sort([17, 99, 42]))
+print(frequency_sort(['bob','bob','carl','alex','bob']))
